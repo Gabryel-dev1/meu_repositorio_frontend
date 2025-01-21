5e29a7138102690cbb9927eadf4832f4dab08d8d
Gabryel Alves <alvesgabryel086@gmail.com>
2025-01-21 11:32:53 -0300

    Salvando projeto

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(numberA, numberB) {
+    return numberA * numberB;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(4, 20));
+console.log(saudacao("Ana"));
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+
+function multiplicar(numberA: number, numberB: number): number {
+    return numberA * numberB;
+}
+
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicar(4, 20));
+
+console.log(saudacao("Ana"));
